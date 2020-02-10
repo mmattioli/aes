f371baa263f9eeccf06792e9655a8fb1764f8d75
Michael Mattioli <mmattioli@users.noreply.github.com>
2020-02-09 23:12:26 -0500

    Implement inverse ShiftRows operation
    Implement the inverse of the ShiftRows operation and add test vectors to
    the testbench to verify results.

--- rtl/aes_shift_rows.vhd
+++ rtl/aes_shift_rows.vhd
@@ -36,6 +36,23 @@ begin
     standard_shift(23 downto 16) <= state(119 downto 112);
     standard_shift(15 downto 8) <= state(79 downto 72);
     standard_shift(7 downto 0) <= state(39 downto 32);
+    
+    inverse_shift(127 downto 120) <= state(127 downto 120);
+    inverse_shift(119 downto 112) <= state(23 downto 16);
+    inverse_shift(111 downto 104) <= state(47 downto 40);
+    inverse_shift(103 downto 96) <= state(71 downto 64);
+    inverse_shift(95 downto 88) <= state(95 downto 88);
+    inverse_shift(87 downto 80) <= state(119 downto 112);
+    inverse_shift(79 downto 72) <= state(15 downto 8);
+    inverse_shift(71 downto 64) <= state(39 downto 32);
+    inverse_shift(63 downto 56) <= state(63 downto 56);
+    inverse_shift(55 downto 48) <= state(87 downto 80);
+    inverse_shift(47 downto 40) <= state(111 downto 104);
+    inverse_shift(39 downto 32) <= state(7 downto 0);
+    inverse_shift(31 downto 24) <= state(31 downto 24);
+    inverse_shift(23 downto 16) <= state(55 downto 48);
+    inverse_shift(15 downto 8) <= state(79 downto 72);
+    inverse_shift(7 downto 0) <= state(103 downto 96);
 
     result <= standard_shift when inverse = '0' else inverse_shift;
 
