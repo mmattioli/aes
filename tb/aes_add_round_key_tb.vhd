c41943a1edefbbdbe0ed9206e7e3dc308eae5a7e
Michael Mattioli <mmattioli@users.noreply.github.com>
2020-02-11 20:45:54 -0500

    Fix minor typos
    Fixed some minor typos in the comments.

--- tb/aes_add_round_key_tb.vhd
+++ tb/aes_add_round_key_tb.vhd
@@ -1,7 +1,7 @@
 --
 -- Written by Michael Mattioli
 --
--- Description: AES AddRoundKey operation.
+-- Description: AES AddRoundKey testbench.
 --
 
 library std;
@@ -21,7 +21,7 @@ architecture structural of aes_add_round_key_tb is
                 round_key   : in std_logic_vector(127 downto 0);
                 result      : out std_logic_vector(127 downto 0));
     end component;
-    
+
     signal state : std_logic_vector(127 downto 0);
     signal round_key : std_logic_vector(127 downto 0);
     signal result : std_logic_vector(127 downto 0);
@@ -41,59 +41,59 @@ begin
         round_key <= x"000102030405060708090a0b0c0d0e0f";
         wait for 1 ns;
         assert (result = x"00102030405060708090a0b0c0d0e0f0");
-        
+
         state <= x"5f72641557f5bc92f7be3b291db9f91a";
         round_key <= x"d6aa74fdd2af72fadaa678f1d6ab76fe";
         wait for 1 ns;
         assert (result = x"89d810e8855ace682d1843d8cb128fe4");
-        
+
         state <= x"ff87968431d86a51645151fa773ad009";
         round_key <= x"b692cf0b643dbdf1be9bc5006830b3fe";
         wait for 1 ns;
         assert (result = x"4915598f55e5d7a0daca94fa1f0a63f7");
-        
+
         state <= x"4c9c1e66f771f0762c3f868e534df256";
         round_key <= x"b6ff744ed2c2c9bf6c590cbf0469bf41";
         wait for 1 ns;
         assert (result = x"fa636a2825b339c940668a3157244d17");
-        
+
         state <= x"6385b79ffc538df997be478e7547d691";
         round_key <= x"47f7f7bc95353e03f96c32bcfd058dfd";
         wait for 1 ns;
         assert (result = x"247240236966b3fa6ed2753288425b6c");
-        
+
         state <= x"f4bcd45432e554d075f1d6c51dd03b3c";
         round_key <= x"3caaa3e8a99f9deb50f3af57adf622aa";
         wait for 1 ns;
         assert (result = x"c81677bc9b7ac93b25027992b0261996");
-        
+
         state <= x"9816ee7400f87f556b2c049c8e5ad036";
         round_key <= x"5e390f7df7a69296a7553dc10aa31f6b";
         wait for 1 ns;
         assert (result = x"c62fe109f75eedc3cc79395d84f9cf5d");
-        
+
         state <= x"c57e1c159a9bd286f05f4be098c63439";
         round_key <= x"14f9701ae35fe28c440adf4d4ea9c026";
         wait for 1 ns;
         assert (result = x"d1876c0f79c4300ab45594add66ff41f");
-        
+
         state <= x"baa03de7a1f9b56ed5512cba5f414d23";
         round_key <= x"47438735a41c65b9e016baf4aebf7ad2";
         wait for 1 ns;
         assert (result = x"fde3bad205e5d0d73547964ef1fe37f1");
-        
+
         state <= x"e9f74eec023020f61bf2ccf2353c21c7";
         round_key <= x"549932d1f08557681093ed9cbe2c974e";
         wait for 1 ns;
         assert (result = x"bd6e7c3df2b5779e0b61216e8b10b689");
-        
+
         state <= x"7ad5fda789ef4e272bca100b3d9ff59f";
         round_key <= x"13111d7fe3944a17f307a78b4d2b30c5";
         wait for 1 ns;
         assert (result = x"69c4e0d86a7b0430d8cdb78070b4c55a");
-        
+
         finish(0);
-        
+
     end process stimuli;
-    
+
 end structural;
--- tb/aes_sub_bytes_tb.vhd
+++ tb/aes_sub_bytes_tb.vhd
@@ -1,7 +1,7 @@
 --
 -- Written by Michael Mattioli
 --
--- Description: AES SubBytes testbcnch.
+-- Description: AES SubBytes testbench.
 --
 
 library std;
@@ -21,7 +21,7 @@ architecture behavioral of aes_sub_bytes_tb is
                 inverse : in std_logic;
                 result  : out std_logic_vector(127 downto 0));
     end component;
-    
+
     signal state : std_logic_vector(127 downto 0);
     signal inverse : std_logic;
     signal result : std_logic_vector(127 downto 0);
@@ -32,99 +32,99 @@ begin
     uut: aes_sub_bytes port map (   state => state,
                                     inverse => inverse,
                                     result => result);
- 
+
     -- Apply the stimuli.
     stimuli : process
     begin
-    
+
         -- Standard operation.
         inverse <= '0';
-        
+
         state <= x"00102030405060708090a0b0c0d0e0f0";
         wait for 1 ns;
         assert (result = x"63cab7040953d051cd60e0e7ba70e18c");
-        
+
         state <= x"89d810e8855ace682d1843d8cb128fe4";
         wait for 1 ns;
         assert (result = x"a761ca9b97be8b45d8ad1a611fc97369");
-        
+
         state <= x"4915598f55e5d7a0daca94fa1f0a63f7";
         wait for 1 ns;
         assert (result = x"3b59cb73fcd90ee05774222dc067fb68");
-        
+
         state <= x"fa636a2825b339c940668a3157244d17";
         wait for 1 ns;
-        assert (result = x"2dfb02343f6d12dd09337ec75b36e3f0");        
-        
+        assert (result = x"2dfb02343f6d12dd09337ec75b36e3f0");
+
         state <= x"247240236966b3fa6ed2753288425b6c";
         wait for 1 ns;
         assert (result = x"36400926f9336d2d9fb59d23c42c3950");
-        
+
         state <= x"c81677bc9b7ac93b25027992b0261996";
         wait for 1 ns;
         assert (result = x"e847f56514dadde23f77b64fe7f7d490");
-        
+
         state <= x"c62fe109f75eedc3cc79395d84f9cf5d";
         wait for 1 ns;
         assert (result = x"b415f8016858552e4bb6124c5f998a4c");
-        
+
         state <= x"d1876c0f79c4300ab45594add66ff41f";
         wait for 1 ns;
         assert (result = x"3e175076b61c04678dfc2295f6a8bfc0");
-        
+
         state <= x"fde3bad205e5d0d73547964ef1fe37f1";
         wait for 1 ns;
         assert (result = x"5411f4b56bd9700e96a0902fa1bb9aa1");
-        
+
         state <= x"bd6e7c3df2b5779e0b61216e8b10b689";
         wait for 1 ns;
         assert (result = x"7a9f102789d5f50b2beffd9f3dca4ea7");
-        
+
         -- Inverse operation.
         inverse <= '1';
-        
+
         state <= x"7a9f102789d5f50b2beffd9f3dca4ea7";
         wait for 1 ns;
         assert (result = x"bd6e7c3df2b5779e0b61216e8b10b689");
-        
+
         state <= x"5411f4b56bd9700e96a0902fa1bb9aa1";
         wait for 1 ns;
         assert (result = x"fde3bad205e5d0d73547964ef1fe37f1");
-        
+
         state <= x"3e175076b61c04678dfc2295f6a8bfc0";
         wait for 1 ns;
         assert (result = x"d1876c0f79c4300ab45594add66ff41f");
-        
+
         state <= x"b415f8016858552e4bb6124c5f998a4c";
         wait for 1 ns;
         assert (result = x"c62fe109f75eedc3cc79395d84f9cf5d");
-        
+
         state <= x"e847f56514dadde23f77b64fe7f7d490";
         wait for 1 ns;
         assert (result = x"c81677bc9b7ac93b25027992b0261996");
-        
+
         state <= x"36400926f9336d2d9fb59d23c42c3950";
         wait for 1 ns;
         assert (result = x"247240236966b3fa6ed2753288425b6c");
-        
+
         state <= x"2dfb02343f6d12dd09337ec75b36e3f0";
         wait for 1 ns;
         assert (result = x"fa636a2825b339c940668a3157244d17");
-        
+
         state <= x"3b59cb73fcd90ee05774222dc067fb68";
         wait for 1 ns;
         assert (result = x"4915598f55e5d7a0daca94fa1f0a63f7");
-        
+
         state <= x"a761ca9b97be8b45d8ad1a611fc97369";
         wait for 1 ns;
         assert (result = x"89d810e8855ace682d1843d8cb128fe4");
-        
+
         state <= x"63cab7040953d051cd60e0e7ba70e18c";
         wait for 1 ns;
         assert (result = x"00102030405060708090a0b0c0d0e0f0");
-        
+
         finish(0);
-    
+
     end process stimuli;
 
 end behavioral;
